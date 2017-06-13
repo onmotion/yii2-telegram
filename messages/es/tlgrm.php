a0dacd95eadb88fe51944610c638e94a89c0cd13
Marcelo Briones <ing@marcelobriones.com.ar>
2017-06-12 23:37:12 -0300

    Spanish translations

---
+++ messages/es/tlgrm.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Message translations.
+ *
+ */
+return [
+    'At the moment, there are no available operators. Please, try to write later.' => 'En este momento, no hay operadores disponibles. Intente más tarde.',
+    'Start conversation with chat ' => 'Iniciar conversación por chat ',
+    'Start conversation' => 'Iniciar conversación',
+    'Login to the support system' => 'Ingresar al sistema de soporte',
+    'Conversation already in progress in this chat. Responsible: ' => 'Conversación ya en progreso en este chat. Responsable: ',
+    'Seems conversation already in progress in this chat.' => 'Parece que la conversación ya está en progreso en este chat.',
+    'Unknown command.' => 'Comando desconocido.',
+    "Passphrase is correct, now you'll get the messages." => 'La contraseña es correcta, ahora recibirá los mensajes.',
+    "You are already subscribed to receive messages." => 'Ya está suscripto para recibir mensajes.',
+    'Incorrect passphrase.' => 'Contraseña incorrecta.',
+    'Try to enter the command, such as /help' => 'Intente ingresar el comando, tal como /help',
+    'Show bot commands help' => 'Muestra la ayuda de comandos',
+    'End the currently active conversation and switch to standby mode.' => 'Finaliza la conversación activa y cambia al modo de espera.',
+    'You are not authorized!' => 'Usted no está autorizado!',
+    'Completed conversation in chat ' => 'Conversación completada por chat ',
+    'You have no active conversations.' => 'Usted no tiene ninguna conversación activa.',
+    'Enter passphrase:' => 'Ingrese contraseña:',
+    'You are already logged in as ' => 'Ya ingresó como ',
+    'You are not logged in.' => 'No ha ingresado.',
+    'You will no longer receive messages.' => 'Ya no recibirá mensajes.',
+    'Logout from the support system.' => 'Salir del sistema de soporte.',
+    'Online support' => 'Soporte en línea',
+    " writes:" => ' escribe:'
+];

5416755fe1744d197e34114c37d1003fd23f4c92
Marcelo Briones <ing@marcelobriones.com.ar>
2017-06-14 01:47:14 -0300

    Add header caption

--- views/default/chat.php
+++ views/default/chat.php
@@ -8,7 +8,12 @@
 use yii\helpers\Html;
 
 echo Html::beginTag('div', ['id' => 'tlgrm-chat']);
-echo Html::tag('div', Html::tag('div', '',['id' => 'tlgrm-close-btn']),['id' => 'tlgrm-chat-head']);
+echo Html::beginTag('div', ['id' => 'tlgrm-chat-head']);
+echo Html::beginTag('div', ['id' => 'tlgrm-chat-head-caption']);
+echo '<i class="glyphicon glyphicon-send"></i> <span>' . Yii::t('tlgrm', 'Online support') . '</span>';
+echo Html::endTag('div');
+echo Html::tag('div', '',['id' => 'tlgrm-close-btn']);
+echo Html::endTag('div');
 echo Html::tag('div', '',['id' => 'tlgrm-chat-flow']);
 echo Html::beginTag('div', ['id' => 'tlgrm-chat-send-panel']);
 echo Html::beginForm('/telegram/chat/send-msg', 'post', ['id' => 'tlgrm-chat-form']);
